a4170f37dbac666bb2349a87ac56a91067cded24
msmannan00 <msmannan00@gmail.com>
2020-08-16 00:16:33 +0500

    bug fixes

--- app/src/main/java/com/darkweb/genesisvpn/application/constants/status.java
+++ app/src/main/java/com/darkweb/genesisvpn/application/constants/status.java
@@ -6,7 +6,7 @@ import com.darkweb.genesisvpn.application.serverManager.serverListRowModel;
 import java.util.ArrayList;
 
 public class status {
-    public static boolean DEVELOPER_MODE = true;
+    public static boolean DEVELOPER_MODE = false;
     public static boolean HAS_APPLICATION_STOPPED = false;
     public static ArrayList<String> DISABLED_APPS = new ArrayList<>();
     public static ArrayList<serverListRowModel> RECENT_SERVERS = new ArrayList<>();
